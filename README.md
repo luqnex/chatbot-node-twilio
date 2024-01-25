c509588965d80d69ce00d6ecdea282e5b4f50c47
Lucas Costa <lucas.costa@pdsolues.com.br>
2024-01-25 08:46:17 -0300

    feat: :sparkles: initial project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Chatbot utilizando twilio e API nodejs
+
+<p>1° - Clone o repositorio utilizando o seu terminal de preferencia</p>
+
+```bash
+   git clone link
+```
+
+<p>2° - Abra a pasta chatbot-node-twilio</p>
+
+```bash
+   cd chatbot-node-twilio
+```
+
+<p>3° - Instale as dependencias e incie o projeto</p>
+
+```bash
+   npm install && npm start
+```
+
+<p>4° - Iniciar o servidor ngrok</p>
+
+<p>5° - Pegar url https do ngrok</p>
+
+<p>5° - Colocar no console da twilio => Messaging => Send a Whastapp message => Sandbox settings.</p>
+
+## Referencia:
+
+<a href="https://www.twilio.com/pt-br/blog/como-criar-um-bot-no-whatsapp-em-5-minutos-em-node-js" target="_blank">https://www.twilio.com/pt-br/blog/como-criar-um-bot-no-whatsapp-em-5-minutos-em-node-js</a>
---
+++ src/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const twilio = require("twilio");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// aqui vamos inserir a lógica do jogo
+app.get("/", (req, res) => {
+  res.send("server started!");
+});
+
+app.post("/message", (req, res) => {
+  console.log("nova mensagem", req.body.Body);
+
+  const userMessage = req.body.Body.toLowerCase();
+
+  const twiml = new twilio.twiml.MessagingResponse();
+
+  switch (userMessage) {
+    case "1":
+      twiml.message("Aguarde um instante, nosso técnico já vai te atender");
+      res.send(twiml.toString());
+      break;
+    case "2":
+      twiml.message("Nossos planos são...");
+      res.send(twiml.toString());
+      break;
+    case "3":
+      twiml.message("Seu boleto é...");
+      res.send(twiml.toString());
+      break;
+    case "4":
+      twiml.message("Finalizando conversa, obrigado pelo contato!");
+      res.send(twiml.toString());
+      break;
+
+    default:
+      twiml.message(`
+Olá, seja bem-vindo ao Assistente Virtual da *LUCAS INTERNET*
+
+Como posso te ajudar? Digite o *NUMERO* da opção desejada:
+    
+1 - Suporte Técnico
+2 - Conhecer planos
+3 - 2ª via de boleto
+4 - Finalizar
+    `);
+
+      res.send(twiml.toString());
+  }
+});
+
+app.listen(3000, function () {
+  console.log("Servidor ativo na porta 3000!");
+});
